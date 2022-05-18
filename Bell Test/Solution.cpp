151fd4f8d140023450880804a175052dc86ccd0c
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2022-05-19 01:55:15 +0530

    Create Solution.cpp

---
+++ Bell Test/Solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a,b,c;
+   cin>>a>>b>>c;
+   if(a>b && a>c)
+   {
+   cout<<"Naruto\n";
+   }
+   else if(b>a && b>c)
+   {
+   cout<<"Sasuke\n";
+   }
+   else
+   {
+   cout<<"Sakura\n";
+   }
+}
